90bebd590210f892be5666d12d74c875c4e795d1
Cristobal Fallas Mora <cristobalfallascr@hotmail.com>
2021-01-19 20:27:48 -0600

    add login controller

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,6 +1,5 @@
 // this controller will have all middleware functions required by the users
 
-const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
@@ -69,17 +68,30 @@ const signup = async (req, res, next) => {
   });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid credentials provided ", 401);
+    return next(new HttpError("Invalid credentials provided ", 401));
   }
   const { email, password } = req.body;
-  const identifiedUser = DUMMY_USERS.find((user) => user.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError("Invalid Credentials, Not identified", 401);
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Login, try again later", 500);
+    return next(error);
   }
+
+  if(!existingUser || existingUser.password !== password){
+    const error = new HttpError(' invalid credentials', 401);
+    return next(error);
+
+  }
+
+  
+
   res.json({ message: "logged in successfully" });
 };
 
